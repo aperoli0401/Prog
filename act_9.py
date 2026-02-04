21e6d9150be48dd3a671d4b19e9978ced8bf91d3
aperoli0401 <aperoli0401@g.educaand.es>
2026-02-04 12:23:27 +0100

    Delete act_9.py

--- act_9.py
+++
@@ -1,9 +0,0 @@
-nombres = ("pablo", "jose", "manuel")
-
-
-print("Los nombres son: ", nombres)
-
-añadir = input("Dime el nombre de otro alumno: ")
-nombres.apend(añadir)
-
-print("Los nuevos nombres son: ", añadir)
