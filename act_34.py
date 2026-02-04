42e2b22945bfc7e7c76bc5743a1fc5531b090ab1
aperoli0401 <aperoli0401@g.educaand.es>
2026-02-04 12:50:14 +0100

    Delete act_34.py

--- act_34.py
+++
@@ -1,9 +0,0 @@
-def cuentaVocales(texto):
-    cuenta = texto.count("aeiou") + texto.count("AEIOU")
-    nuevoTexto = texto.replace("aeiou","x")
-    nuevoTexto2 = texto.replace("AEIOU","X")
-    return nuevoTexto2
-
-frase = input("Dime una frase o una palabra: ")
-print(cuentaVocales(nuevoTexto2))
-    
