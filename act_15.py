a50aa0b5d24b25d81b1f391c8f6926ab4f421575
aperoli0401 <aperoli0401@g.educaand.es>
2026-02-04 12:36:45 +0100

    Delete act_15.py

--- act_15.py
+++
@@ -1,8 +0,0 @@
-def busquedaLineal (lista, n):
-    i = 0
-    respuesta = -1
-    while i < len(lista) and respuesta == -1:
-        if lista[i] == n:
-            respuesta = i
-        i = i + 1 
-    return(respuesta)
